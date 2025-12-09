52dbf14399bf97a35564fafdd59a92922b861147
J <joao.adsbrasil@gmail.com>
2025-12-09 01:46:32 -0300

    O que são Variáveis

---
+++ variaveis.js
@@ -0,0 +1,12 @@
+/* 
+Variaveis
+
+    - let -> PODE ALTERAR O VALOR
+    - const -> VALOR NÃO PODE SER DECLARADO NOVAMENTE
+    - var -> DESCONTINUADO / NÃO USAR
+
+*/
+
+let abacate = 40
+
+console.log(abacate)
