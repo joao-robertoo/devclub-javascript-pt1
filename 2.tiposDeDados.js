3e24e6f7af78290b40766d8ae0544e779dce268b
J <joao.adsbrasil@gmail.com>
2025-12-09 01:59:06 -0300

    Strings

---
+++ 2.tiposDeDados.js
@@ -0,0 +1,15 @@
+/* 
+
+1. String -> Textos
+
+*/
+
+const texto1 = "Textos com aspas 'duplas'" /* não consigo pular linhas */
+const texto2 = 'Textos com aspas "simples"' /* não consigo pular linhas */
+const texto3 = `Textos com a crase` /* consigo pular linhas */
+
+console.log(texto1);
+console.log(texto2);
+console.log(texto3);
+
+
