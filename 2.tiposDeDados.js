92625042456f74f1203a5e5dd7f57154d35d3a18
J <joao.adsbrasil@gmail.com>
2025-12-09 21:17:02 -0300

    Array

--- 2.tiposDeDados.js
+++ 2.tiposDeDados.js
@@ -6,12 +6,31 @@
 4. Object -> Objetos
 5. Null & Undefined
 
+Esrutura de Dados
+
+1. Array
+
 */
 
-const user = { 
-    name: "Joao",
-    age: 30,
-    nomeDoConjuge: "Jessica",
-    id: null
-}
-console.log(user.id)
+const myArray = [
+    {
+        nome: 'Joao',
+        idade: 30,
+        nomeDoConjuge: 'Jessica',
+        id: null,
+    },
+    {
+        nome: 'Joao',
+        idade: 22,
+        nomedoConjuge: 'Joao',
+        id: null,
+    },
+]
+
+const users = [20, 30, 490]
+
+console.log(users[2])
+
+
+
+
